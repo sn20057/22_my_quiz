6d68ebac1c7c9b3073a986a807b93a3ef1eb3a30
sn20057 <sn20057@melville-high.school.nz>
2022-09-29 10:00:04 +1300

    Generates 15 random multiplication questions Has 3 live, you lose a life if you get question wrong lose all your lives, you lose Displays the round 1 from 15 If question wrong it displays correct answer

--- 00_my_quiz.py
+++ 00_my_quiz.py
@@ -2,11 +2,11 @@ import random
 # functions here
 
 
-def yes_no(question):
+def yes_no(question2):
     valid = False
     while not valid:
         # ask user for input
-        response = input(question).lower().strip()
+        response = input(question2).lower().strip()
         # if no the instructions are displayed
         if response == "no" or response == "n":
             response = "No"
@@ -29,9 +29,8 @@ def instructions():
 
 
 # main routine here
-print("Welcome to my quiz game")
+print("Welcome to my quiz\n")
 
-print()
 played_before = yes_no("Have you played this game before? ")
 
 # if user hasn't played display the instructions
@@ -39,5 +38,50 @@ if played_before == "No":
     instructions()
 
 rounds_played = 0
+rounds = 15
+lives = 3
+error = "Please enter a whole digit"
 
+for item in range(0, 5):
+    # Creates the random numbers for each multiplication from 1 to 12
+    math_num1 = random.randint(1, 12)
+    math_num2 = random.randint(1, 12)
 
+    # questions 1 to 15
+    start = "Press <Enter> to start play\n"
+    answer = math_num1 * math_num2
+    question = int(input("What is {} x {} = ".format(math_num1, math_num2)))
+    lives -= 1
+    rounds -= 1
+
+    # If user answer correct
+    if question == answer:
+        print("Correct\n")
+        # They keep their life and displays how many they have left
+        lives += 1
+        print("Lives {} \n".format(lives))
+        # Display's how many rounds are left in the quiz
+        print("{}/15 Round's Remaining ".format(rounds))
+
+    # If user is incorrect
+    elif question != answer:
+        print("Incorrect\n")
+        # shows the right answer for the question
+        print("Answer is {} \n".format(answer))
+        # Display's remaining lives
+        print("Lives Remaining {} ".format(lives))
+        # Shows the rounds remaining
+        print("{}/15 Round's Remaining \n".format(rounds))
+        # If lives are equal to 0 the game ends and the user loses
+        if lives == 0:
+            print("you have run out of lives")
+            break
+
+    # If rounds are equal to 0 the game ends the user wins
+    if rounds == 0:
+        # Congratulations message
+        print("You Have Completed The Quiz Successfully")
+        # Shows how many the user has remaining
+        print("Your Remaining Lives Are {}".format(lives))
+        # A thankyou message
+        print("Thank for playing my Quiz :)")
