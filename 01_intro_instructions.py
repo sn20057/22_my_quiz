881eb392b43b50027f1b1fc29ca355291444c7c0
sn20057 <sn20057@melville-high.school.nz>
2022-10-17 09:13:19 +1300

    Instructions to my quiz

--- 01_intro_instructions.py
+++ 01_intro_instructions.py
@@ -18,20 +18,21 @@ def yes_no(question):
 
 # instructions function on how to play
 def instructions():
-    print("- - - *HOW TO PLAY* - - -")
-    print()
-    print("The rules of the game here")
-    print()
+    print("HOW TO PLAY")
+    print("The rules of the game here\n")
+    print("There are 15 random multiplication questions")
+    print("You have 3 lives, if you get one question wrong you\n"
+          "Lose a life, you get one right you keep that life")
+    print("If you lose your 3 lives you lose. If you\n"
+          "Survive the 15 rounds you win\n")
     return""
 
 
 # Main routine goes here
 
-print("Welcome to my quiz game")
+print("Welcome to my quiz game\n")
 
-print()
-
-played_before = yes_no("Have you played this game before? ")
+played_before = yes_no("Have you played this game before?\n ")
 
 # if user hasn't played display the instructions
 if played_before == "No":
